d461bda234a266f0ff0e3a8700d19b5b7a4122a0
mGuv <mattmcg.gamedev@gmail.com>
2020-02-06 19:53:58 +0000

    Add sorting to all columns on Head page

--- src/Pages/HeadPage.tsx
+++ src/Pages/HeadPage.tsx
@@ -17,7 +17,31 @@ class HeadPage extends Page<PageProps, PageState>
 
         return (a.Head?.Attack < b.Head?.Attack) ? 1 : -1;
     }
-    
+
+    private sortByDurability(a:Material, b:Material):number {
+        if(a.Head?.Durability === b.Head?.Durability) {
+            return 0;
+        }
+
+        return (a.Head?.Durability < b.Head?.Durability) ? 1 : -1;
+    }
+
+    private sortByHarvestLevel(a:Material, b:Material):number {
+        if(a.Head?.HarvestLevel === b.Head?.HarvestLevel) {
+            return 0;
+        }
+
+        return (a.Head?.HarvestLevel < b.Head?.HarvestLevel) ? 1 : -1;
+    }
+
+    private sortByHarvestSpeed(a:Material, b:Material):number {
+        if(a.Head?.MiningSpeed === b.Head?.MiningSpeed) {
+            return 0;
+        }
+
+        return (a.Head?.MiningSpeed < b.Head?.MiningSpeed) ? 1 : -1;
+    }
+
     public render(): JSX.Element {
         return (
             <div>
@@ -27,9 +51,9 @@ class HeadPage extends Page<PageProps, PageState>
                         <tr>
                             <th>Material</th>
                             <th onClick={() => {this.orderMaterials(this.sortByAttack)}}>Attack</th>
-                            <th>Durability</th>
-                            <th>Harvest Level</th>
-                            <th>Harvest Speed</th>
+                            <th onClick={() => {this.orderMaterials(this.sortByDurability)}}>Durability</th>
+                            <th onClick={() => {this.orderMaterials(this.sortByHarvestLevel)}}>Harvest Level</th>
+                            <th onClick={() => {this.orderMaterials(this.sortByHarvestSpeed)}}>Harvest Speed</th>
                         </tr>
                     </thead>
                     <tbody>
