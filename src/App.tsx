806f4498b9c76227b86f2b7ad38d7923caf0ae8d
mGuv <mattmcg.gamedev@gmail.com>
2020-02-06 19:19:40 +0000

    Make app use Page Components

--- src/App.tsx
+++ src/App.tsx
@@ -2,214 +2,41 @@ import React from 'react';
 import MaterialService from './Materials/MaterialService';
 import Material from './Materials/Material';
 import BowPage from './Pages/BowPage';
+import PageProps from './Pages/PageProps';
+import HeadPage from './Pages/HeadPage';
 
 interface Props {
 
 }
 
 interface State {
-  materials: Material[]
-  selectedMaterial?: string
+  materials: Material[],
+  selectedPage: React.ComponentType<PageProps>
 }
 
 class App extends React.PureComponent<Props, State> {
 
+  private pages:React.ComponentType<PageProps>[];
+
   public constructor(props: Props) {
     super(props);
+    this.pages=[
+      BowPage,
+      HeadPage
+    ];
     this.state = {
-      materials: MaterialService.GetInstance().GetAll()
+      materials: MaterialService.GetInstance().GetAll(),
+      selectedPage: this.pages[0]
     };
 
-  }
-
-  private sortByModifier()
-  {
-    let mats:Material[] = Array.from(this.state.materials);
-    mats = mats.sort((a:Material, b:Material)=>{
-      if(!a.Handle && !b.Handle) {
-        return 0;
-      }
-
-      if(!b.Handle) {
-        return -1;
-      }
-
-      if(!a.Handle) {
-        return 1;
-      }
-
-
-        if(a.Handle.Modifier === b.Handle.Modifier) {
-          return 0;
-        }
-        return (a.Handle.Modifier < b.Handle.Modifier) ? 1 : -1;
-    });
-
-    console.log("setting state");
-    this.setState({
-      ...this.state,
-      materials: mats
-    });
   }
 
   public render(): JSX.Element {
     return (
       <div>
-        <BowPage allMaterials={this.state.materials}/>
-        {/* <h1>Bow Stats</h1>
         {
-          <table>
-          <thead>
-            <tr>
-              <th>Material</th>
-              <th>Draw Speed</th>
-              <th>Bonus Damage</th>
-              <th>Range</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-            this.state.materials.map((material, index) => {
-                if(material.Bow) {
-                  return (<tr>
-                    <td>{material.Name}</td>
-                    <td>{material.Bow.DrawSpeed}</td>
-                    <td>{material.Bow.BonusDamage}</td>
-                    <td>{material.Bow.Range}</td>
-                  </tr>);
-                }
-            })
-          }
-          </tbody>
-          </table>
+          <this.state.selectedPage allMaterials={this.state.materials}/>
         }
-        <h1>Extra Stats</h1>
-        {
-          <table>
-          <thead>
-            <tr>
-              <th>Material</th>
-              <th>Bonus Durability</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-            this.state.materials.map((material, index) => {
-                if(material.Extra) {
-                  return (<tr>
-                    <td>{material.Name}</td>
-                    <td>{material.Extra.ExtraDurability}</td>
-                  </tr>);
-                }
-            })
-          }
-          </tbody>
-          </table>
-        }
-        <h1>Fletching Stats</h1>
-        {
-          <table>
-          <thead>
-            <tr>
-              <th>Material</th>
-              <th>Accuracy</th>
-              <th>Modifier</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-            this.state.materials.map((material, index) => {
-                if(material.Fletching) {
-                  return (<tr>
-                    <td>{material.Name}</td>
-                    <td>{material.Fletching.Accuracy}</td>
-                    <td>{material.Fletching.Modifier}</td>
-                  </tr>);
-                }
-            })
-          }
-          </tbody>
-          </table>
-        }
-        <h1>Shaft Stats</h1>
-        {
-          <table>
-          <thead>
-            <tr>
-              <th>Material</th>
-              <th>Bonus Ammo</th>
-              <th>Modifier</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-            this.state.materials.map((material, index) => {
-                if(material.Shaft) {
-                  return (<tr>
-                    <td>{material.Name}</td>
-                    <td>{material.Shaft.BonusAmmo}</td>
-                    <td>{material.Shaft.Modifier}</td>
-                  </tr>);
-                }
-            })
-          }
-          </tbody>
-          </table>
-        }
-        <h1>Head Stats</h1>
-        {
-          <table>
-          <thead>
-            <tr>
-              <th>Material</th>
-              <th>Attack</th>
-              <th>Durability</th>
-              <th>Harvest Level</th>
-              <th>Mining Speed</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-            this.state.materials.map((material, index) => {
-                if(material.Head) {
-                  return (<tr>
-                    <td>{material.Name}</td>
-                    <td>{material.Head.Attack}</td>
-                    <td>{material.Head.Durability}</td>
-                    <td>{material.Head.HarvestLevel}</td>
-                    <td>{material.Head.MiningSpeed}</td>
-                  </tr>);
-                }
-            })
-          }
-          </tbody>
-          </table>
-        }
-        <h1>Handle Stats</h1>
-        {
-          <table>
-            <thead>
-              <tr>
-                <th>Material</th>
-                <th>Durability</th>
-                <th onClick={this.sortByModifier.bind(this)}>Modifier</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-              this.state.materials.map((material, index) => {
-                  if(material.Handle) {
-                    return (<tr>
-                      <td>{material.Name}</td>
-                      <td>{material.Handle.Durability}</td>
-                      <td>{material.Handle.Modifier}</td>
-                    </tr>);
-                  }
-              })
-            }
-            </tbody>
-            </table>
-        } */}
       </div>
     );
   }
--- src/Pages/HeadPage.tsx
+++ src/Pages/HeadPage.tsx
@@ -4,23 +4,30 @@ import Page from './Page';
 import PageProps from './PageProps';
 import PageState from './PageState';
 
-class BowPage extends Page<PageProps, PageState>
+class HeadPage extends Page<PageProps, PageState>
 {
     public constructor(props: PageProps) {
-        super(props, (m:Material) => {return m.Bow !== undefined});
+        super(props, (m:Material) => {return m.Head !== undefined});
     }
 
+    /**
+     * public readonly Attack:number;
+    public readonly Durability:number;
+    public readonly HarvestLevel:number;
+    public readonly MiningSpeed:number;
+     */
     public render(): JSX.Element {
         return (
             <div>
-                <h1>Bow Page</h1>
+                <h1>Head Page</h1>
                 <table>
                     <thead>
                         <tr>
                             <th>Material</th>
-                            <th>Draw Speed</th>
-                            <th>Damage</th>
-                            <th>Range</th>
+                            <th>Attack</th>
+                            <th>Durability</th>
+                            <th>Harvest Level</th>
+                            <th>Harvest Speed</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -29,9 +36,10 @@ class BowPage extends Page<PageProps, PageState>
                                     return (
                                         <tr>
                                             <td>{m.Name}</td>
-                                            <td>{m.Bow?.DrawSpeed}</td>
-                                            <td>{m.Bow?.BonusDamage}</td>
-                                            <td>{m.Bow?.Range}</td>
+                                            <td>{m.Head?.Attack}</td>
+                                            <td>{m.Head?.Durability}</td>
+                                            <td>{m.Head?.HarvestLevel}</td>
+                                            <td>{m.Head?.MiningSpeed}</td>
                                         </tr>
                                     );
                             })
@@ -43,4 +51,4 @@ class BowPage extends Page<PageProps, PageState>
     }
 }
 
-export default BowPage;
+export default HeadPage;
