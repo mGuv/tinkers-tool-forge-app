25b154c5aef93c6e06bdd2d24db33d786fd49cf8
mGuv <mattmcg.gamedev@gmail.com>
2020-02-09 19:45:58 +0000

    Add a basic text filter to the Materials page

--- src/PartViewer/MaterialList.tsx
+++ src/PartViewer/MaterialList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, RefObject } from 'react';
 import SortableTable from '../SortableTable';
 import Material from '../Materials/Material';
 
@@ -8,14 +8,40 @@ interface Props {
     toggleMaterial: (material:Material) => void,
 }
 
-const MaterialList: React.FunctionComponent<Props> = ({ materials, toggleMaterial }) => (
-    <div>
-        <SortableTable columnInfo={[["Colour", false], "Name", ["Visibility", false]]} data={materials.map(material => ({
-            Colour: <div style={{width: "16px", height: "16px", backgroundColor: "#" + material.Color}}/>,
-            Name: material.Name,
-            "Visibility": <i onClick={() => {toggleMaterial(material)}} className={material.Hidden ? 'fas fa-eye-slash' :'fas fa-eye'}/>
-        }))} />
-    </div>
-)
+interface State {
+    materialFilter:string,
+}
+
+class MaterialList extends React.PureComponent<Props, State> {
+    public constructor(props:Props) {
+        super(props);
+
+        this.state = {
+            materialFilter: ""
+        };
+    }
+
+    private updateFilter(event:ChangeEvent<HTMLInputElement>) {
+        event.stopPropagation();
+
+        this.setState({
+            ...this.state,
+            materialFilter: event.target.value
+        });
+    }
+
+    public render() {
+        return (
+            <div>
+                <div><i className="fas fa-search"/><input onChange={this.updateFilter.bind(this)} autoFocus placeholder="Enter Material..."/></div>
+                <SortableTable columnInfo={[["Colour", false], "Name", ["Visibility", false]]} data={this.props.materials.filter(material => material.Name.indexOf(this.state.materialFilter) >= 0).map(material => ({
+                    Colour: <div style={{width: "16px", height: "16px", backgroundColor: "#" + material.Color}}/>,
+                    Name: material.Name,
+                    "Visibility": <i onClick={() => {this.props.toggleMaterial(material)}} className={material.Hidden ? 'fas fa-eye-slash' :'fas fa-eye'}/>
+                }))} />
+            </div>
+        );
+    }
+}
 
 export default MaterialList;
