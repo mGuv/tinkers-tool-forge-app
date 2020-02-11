69882838bfef257a2783713209fa572f0734a77b
mGuv <mattmcg.gamedev@gmail.com>
2020-02-11 18:38:47 +0000

    Fix issues with Part Picker and make it easier to use

--- src/PartSelector/index.tsx
+++ src/PartSelector/index.tsx
@@ -1,28 +1,35 @@
-import React from "react";
+import React, { RefObject } from "react";
 import Part from "../Materials/Parts/Part";
 
 import styles from './styles.module.css';
 
+interface Content<TPart> {
+    label: string,
+    value: (part:TPart) => string
+}
 
-interface Props<TPart extends Part , TData> {
+interface Props<TPart extends Part> {
     parts:TPart[],
-    headers: (keyof TData)[],
-    data: (part:TPart) => TData[],
-    selectPart: (part:Part) => void
+    selectPart: (part:TPart) => void,
+    content: Content<TPart>[]
 }
 
 interface State {
     isOpen: boolean
 }
 
-class PartSelector<TPart extends Part, TData> extends React.PureComponent<Props<TPart, TData>, State> 
+class PartSelector<TPart extends Part> extends React.PureComponent<Props<TPart>, State> 
 {    
-    public constructor(props:Props<TPart, TData>)
+    private containerRef: RefObject<HTMLDivElement>;
+
+    public constructor(props:Props<TPart>)
     {
         super(props);
         this.state = {
             isOpen: false
         }
+
+        this.containerRef = React.createRef<HTMLDivElement>();
     }
 
     private open() {
@@ -35,6 +42,7 @@ class PartSelector<TPart extends Part, TData> extends React.PureComponent<Props<
         this.setState({
             isOpen: false
         });
+        this.containerRef.current?.blur();
     }
 
     private onItemSelected(part: TPart) {
@@ -46,17 +54,16 @@ class PartSelector<TPart extends Part, TData> extends React.PureComponent<Props<
     public render(): JSX.Element
     {
         return (
-            <div tabIndex={0} onBlur={this.close.bind(this)} onFocus={this.open.bind(this)}>
+            <div ref={this.containerRef} tabIndex={0} onBlur={this.close.bind(this)} onFocus={this.open.bind(this)}>
                 <div>Material Picker</div>
                 <div>
                 {
                     this.state.isOpen ? (<div className={styles.partList}> {this.props.parts.map(p => {
                         return (<div className={styles.partRow} onClick={() => {this.onItemSelected(p)}}>
                             <div style={{gridColumn: "span 1", width: "16px", height:"16px", backgroundColor: p.Material.Color}}></div><div style={{gridColumn: "span 1"}}>{p.Material.Name}</div>
-                            {this.props.data(p).map(d => 
-                                this.props.headers.map(h =>
-                                    <div style={{gridColumn: "span 1"}}>{h}: {d[h]}</div>)
-                            )}
+                            {
+                                this.props.content.map(c => <div style={{gridColumn: "span 1"}}>{c.label}: {c.value(p)}</div>)
+                            }
                             </div>
                         )}
                     )}</div>) : <React.Fragment/>
